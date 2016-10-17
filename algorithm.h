c528f1a1ff6f3cd3bdf5439a2c19a1946397329b
TravisHoover <traviskhoover@gmail.com>
2016-10-17 18:11:00 -0500

    project changed

---
+++ algorithm.h
@@ -0,0 +1,9 @@
+typedef int DataType;
+
+void bubbleSort(DataType theArray[], int n);
+
+void insertionSort(DataType theArray[], int n);
+
+void mergesort(DataType theArray[], int first, int last);
+
+void quicksort(DataType theArray[], int first, int last);
