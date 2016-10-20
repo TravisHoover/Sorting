d0e7338f8a992df246c2a6d851f08e729fcd383c
PC BOOTCAMP DESKTOP-8IBP5BQ\Travis <traviskhoover@gmail.com>
2016-10-19 22:30:01 -0500

    Added generic definitions for sorting algorithms, headers and main.cpp file, updated gitignore to ignore Visual Studio byproducts

---
+++ ola4.cpp
@@ -0,0 +1,13 @@
+#include "algorithm.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "Array Size\t" << "Bubble\t" << "Insertion\t" << "Merge\t" << "Quick" << endl;
+	cout << "**********\t" << "******\t" << "*********\t" << "*****\t" << "******" << endl;
+
+
+	return 0;
+}
